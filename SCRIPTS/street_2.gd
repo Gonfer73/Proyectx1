8c367a709ee50ec068ef9e2d7c72ad0ee0a46910
Gonfer73 <ferrergonzalo988@gmail.com>
2025-08-13 10:32:17 +0200

    Started to add building of case 1

--- SCRIPTS/street_2.gd
+++ SCRIPTS/street_2.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 
+
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -13,13 +15,13 @@ func _process(_delta):
 
 func _ready():
 	if Blink.dismounting:
+		$player/street_camera/e_icon.show()
 		$police_car.rotation_degrees = Blink.car_rotation
 		$police_car.position = Blink.position_p
 		if Blink.car_rotation < 90 and Blink.car_rotation > -90:
 			$player.position = Blink.position_p + Vector2(0, Blink.car_rotation + 60)
 		else:
 			$player.position = Blink.position_p + Vector2(Blink.car_rotation + 60, 0)
-
 func _on_car_enter_body_entered(_body):
 	$player/street_camera/e_icon.show()
 	Blink.car_enter = true
